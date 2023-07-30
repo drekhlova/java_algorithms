8c79cef69cc53b8307b24221389bb83e5e83130a
Daria Rekhlova <dolly1993@yandex.ru>
2023-07-30 23:16:21 +0400

    Добавили нумерацию

--- lesson4/lesson4/src/main/java/ru/geekbrains/lesson4/Program.java
+++ lesson4/lesson4/src/main/java/ru/geekbrains/lesson4/Program.java
@@ -2,7 +2,7 @@ package ru.geekbrains.lesson4;
 
 
 
-public class Program {
+/*public class Program {
 
     public static void main(String[] args) {
 
@@ -32,4 +32,37 @@ public class Program {
 
 }
 
+*/
 
+public class Program {
+
+    public static void main(String[] args) {
+
+        HashMap<String, String> hashMap = new HashMap<>(4);
+
+        String res = hashMap.put("+79005554433", "Андрей");
+        res = hashMap.put("+79005554432", "Алексей");
+        res = hashMap.put("+79005554432", "Дарья1");
+        res = hashMap.put("+79005554433", "Дарья2");
+        res = hashMap.put("+79005554434", "Дарья3");
+        res = hashMap.put("+79005554435", "Дарья4");
+        res = hashMap.put("+79005554436", "Дарья5");
+        res = hashMap.put("+79005554437", "Дарья6");
+        res = hashMap.put("+79005554438", "Дарья7");
+        res = hashMap.put("+79005554439", "Дарья8");
+
+
+        res = hashMap.get("+79005554436");
+
+        hashMap.remove("+79005554438");
+
+        int indexNumber = 1;
+    
+
+        for (HashMap.Entity element : hashMap) {
+            System.out.println(indexNumber++ + ")" + " " + element.key + " - " + element.value + ";");
+        }
+
+    }
+
+}
